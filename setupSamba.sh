65586dbe043fde2aaac47f2a97fb4299839d809a
rallyeaon <95368092+rallyeaon@users.noreply.github.com>
2023-03-03 17:44:35 +0100

    Update setupSamba.sh

--- setupSamba.sh
+++ setupSamba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # download this script using the command:
-# wget https://raw.githubusercontent.com/rallyeaon/RasPi-Server/master/setupSamba.sh
+# wget https://github.com/rallyeaon/RasPi-Server/blob/main/setupSamba.sh
 #
 # function copied from raspi-config  -  it's updating /boot/config.txt
 
