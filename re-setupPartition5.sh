f4eb84d87450d0f1be2effe25b9bf86ee32135ab
rallyeaon <95368092+rallyeaon@users.noreply.github.com>
2025-01-29 13:34:22 +0100

    Update re-setupPartition5.sh

--- re-setupPartition5.sh
+++ re-setupPartition5.sh
@@ -65,4 +65,48 @@ resize2fs "${device}2"
 
 # show the result of the work performed
 parted -s "$device" print free unit s print free
+
+# Let's add NVMeData and the Share-volume to /etc/fstab to ensure it will be mounted on every boot process:
+# Get name of PARTUUID3 (the data-volume NVMeData)
+PARTUUID3=$(blkid -o value -s PARTUUID /dev/sdb3)
+
+#create a temporary directory to provide a mount-point for the new root-volume
+
+tempdir=$(mktemp -d)
+
+# mount the root-volume to this mount-point
+mount /dev/sdb2 $tempdir
+
+# create a backup of /etc/fstab
+cp -v $tempdir/etc/fstab $tempdir/etc/fstab.ok
+
+# add the relevant line at the end of /etc/fstab
+su -c "echo '# mount the NVMeData-volume' >> $tempdir/etc/fstab"
+su -c "echo 'PARTUUID=$PARTUUID3  /mnt/NVMeData    ext4    defaults         0       2' >> $tempdir/etc/fstab"
+su -c "echo '#' >> $tempdir/etc/fstab"
+
+# now let's add the 2TB-HD attached via USB3 to /etc/fstab
+su -c "echo '# mount share-volume' >> $tempdir/etc/fstab"
+# Attention!! the next line is commented in fstab and needs to be uncommented in case share-volume
+#   shall be mounted. Please doublecheck the PARTUUID of share-volume 
+su -c "echo '#PARTUUID=ce5a4333-c0e8-4f38-a3b1-5d9c80c4ec79 /mnt/share  ext4 defaults   0   2' >> $tempdir/etc/fstab"
+
+# now let's create the subdirectories within /mnt for the mount points
+if [ ! -d "$tempdir/mnt/share" ]; then
+   mkdir $tempdir/mnt/share
+fi
+
+if [ ! -d "$tempdir/mnt/NVMeData" ]; then
+   mkdir $tempdir/mnt/NVMeData
+fi
+
+# SonosSpeak remains on /mnt for historical reasons but is planned to be moved to /mnt/NVMeData
+if [ ! -d "$tempdir/mnt/SonosSpeak" ]; then
+   mkdir $tempdir/mnt/SonosSpeak
+fi
+
+# boot-devise no longer needed, therefore unmount it from our system and remove temporary directory
+umount /dev/sdb2
+rm -r $tempdir
+
 exit
