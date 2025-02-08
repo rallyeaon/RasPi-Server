22edebda38dcd1b0adbcbc9162c891707834b4fb
rallyeaon <95368092+rallyeaon@users.noreply.github.com>
2025-02-08 15:34:52 +0100

    Create restorePROXYData5.sh

---
+++ restorePROXYData5.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script restores configuration data for NGINX and LETSENCRYPT for my Raspi-Backup
+#
+# download this script using the command:
+# wget https://raw.githubusercontent.com/rallyeaon/RasPi-Server/refs/heads/main/restoreCERTData5.sh
+#
+if (( $EUID != 0 )); then
+   echo "$0 must run as 'root'"
+   echo "please restart with correct permissions"
+   exit
+fi
+#
+Remote=sepp@RasPi-Backup
+RecoveryPath=/opt/certbot
+RemotePath=/mnt/BackupDevice/RasPi-Server$RecoveryPath
+
+# retrieve a local copy of the most recent FHEM-Backup
+rsync -4auv --rsync-path="sudo rsync" $Remote:$RemotePath /opt
+
+
+RecoveryPath=/opt/nginx
+RemotePath=/mnt/BackupDevice/RasPi-Server$RecoveryPath
+
+# retrieve a local copy of the most recent FHEM-Backup
+rsync -4auv --rsync-path="sudo rsync" $Remote:$RemotePath /opt
+
+exit
