de44a55fd2ea990a34f5a0d38f9d5577771cc352
rallyeaon <95368092+rallyeaon@users.noreply.github.com>
2025-02-16 11:00:28 +0100

    Update installFHEMbasic.sh

--- installFHEMbasic.sh
+++ installFHEMbasic.sh
@@ -23,6 +23,9 @@ EOF
 # for FHEMpy below modules are to be installed
 sudo apt install -y python3 python3-pip python3-dev python3-venv libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libglib2.0-dev libdbus-1-dev bluez libbluetooth-dev git libprotocol-websocket-perl
 
+# install prerequisites for FritzBox-access
+sudo apt install -y libjson-perl libwww-perl libsoap-lite-perl libjson-xs-perl
+
 # install prerequisites for alexa-fhem
 sudo apt install -y nodejs npm
 # show version of nodejs
