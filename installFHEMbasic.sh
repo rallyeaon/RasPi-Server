3bd3ac720add18f14189cf4c2b92ad82c7f47a17
rallyeaon <95368092+rallyeaon@users.noreply.github.com>
2025-02-17 09:08:45 +0100

    Update installFHEMbasic.sh

--- installFHEMbasic.sh
+++ installFHEMbasic.sh
@@ -27,7 +27,7 @@ sudo apt install -y python3 python3-pip python3-dev python3-venv libffi-dev libs
 sudo apt install -y libjson-perl libwww-perl libsoap-lite-perl libjson-xs-perl
 
 # install prerequisites for Denon-AVR
-sudo apt -y install libxml-simple-perl
+sudo apt -y install libxml-simple-perl libnet-telnet-perl
 
 # install prerequisites for alexa-fhem
 sudo apt install -y nodejs npm
